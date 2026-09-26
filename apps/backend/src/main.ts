00e1bb76da7d4ec6756ff43947e3fe36d9dd0d2d
vanjagigovic <65625302+vanjagigovic@users.noreply.github.com>
2026-09-26 23:08:30 +0200

    feat: harden CORS configuration

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -3,15 +3,20 @@ import { AppModule } from './app.module.js';
 import { ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create(AppModule);
 
-  app.useGlobalPipes(
-  new ValidationPipe({
-    whitelist: true,
-    forbidNonWhitelisted: true,
-    transform: true,
-  }),
-);
-  await app.listen(process.env.PORT ?? 3000);
+    app.enableCors({
+        origin: process.env.FRONTEND_URL,
+        credentials: true,
+    });
+
+    app.useGlobalPipes(
+        new ValidationPipe({
+            whitelist: true,
+            forbidNonWhitelisted: true,
+            transform: true,
+        }),
+    );
+    await app.listen(process.env.PORT ?? 3000);
 }
 await bootstrap();
